fbf549bdac480fc5f0414180c4533f3ad05afb07
vanyarahma <rahmafadilavanya.com>
2025-12-21 10:44:01 +0700

    fix: disable prerender for create invoice page

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,10 +1,12 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
- 
+
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const customers = await fetchCustomers();
- 
+
   return (
     <main>
       <Breadcrumbs
