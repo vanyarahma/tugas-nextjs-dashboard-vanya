41e7fd1529f6633f916d03e1264d5505a3d84b64
vanyarahma <rahmafadilavanya.com>
2025-12-21 11:37:40 +0700

    fix

--- auth.config.ts
+++ auth.config.ts
@@ -1,27 +1,21 @@
 import type { NextAuthConfig } from 'next-auth';
-
+ 
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
-  callbacks: {
-    authorized({ auth, request }) {
+callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const pathname = request.nextUrl.pathname;
-      const isOnCustomers = pathname.startsWith('/customers');
-
-      if (isOnCustomers) {
-        return isLoggedIn;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
-
-      if (isLoggedIn) {
-        return Response.redirect(
-          new URL('/customers', request.nextUrl)
-        );
-      }
-
       return true;
     },
   },
-  providers: [],
+  providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
