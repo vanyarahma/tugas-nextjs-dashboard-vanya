7e3c10880977415cc008747a45514858989193ac
vanyarahma <rahmafadilavanya.com>
2025-12-21 11:28:34 +0700

    Fix auth.config syntax error

--- auth.config.ts
+++ auth.config.ts
@@ -1,25 +1,27 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
-  authorized({ auth, request }) {
-    const isLoggedIn = !!auth?.user;
-    const pathname = request.nextUrl.pathname;
-    const isOnCustomers = pathname.startsWith('/customers');
+    authorized({ auth, request }) {
+      const isLoggedIn = !!auth?.user;
+      const pathname = request.nextUrl.pathname;
+      const isOnCustomers = pathname.startsWith('/customers');
 
-    if (isOnCustomers) {
-      return isLoggedIn; // belum login → auto ke /login
-    }
+      if (isOnCustomers) {
+        return isLoggedIn;
+      }
 
-    if (isLoggedIn) {
-      return Response.redirect(
-        new URL('/customers', request.nextUrl)
-      );
-    }
+      if (isLoggedIn) {
+        return Response.redirect(
+          new URL('/customers', request.nextUrl)
+        );
+      }
 
-    return true;
+      return true;
+    },
   },
-},
+  providers: [],
+} satisfies NextAuthConfig;
